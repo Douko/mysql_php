e46572f09b29f93eed276ce196a8ca120817559f
Douko <bryanpq3237@gmail.com>
2025-10-28 01:50:22 -0500

    changes

--- Ch6/6-15.php
+++ Ch6/6-15.php
@@ -1,7 +1,7 @@
 <?php
     $j       = 23;
     $temp    = "Hello";
-    $address = "1 Old Street";
+    $address = "1 Old Street hello";
     $age     = 61;
     print_r(compact(explode(' ', 'j temp address age')));
 ?>
