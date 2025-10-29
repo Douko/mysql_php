1aa8804344a88c1faadcd447896c89cdce7b8380
Douko <bryanpq3237@gmail.com>
2025-10-29 17:46:38 -0500

    changesm

---
+++ Ch7/7-11.php
@@ -0,0 +1,10 @@
+<?php 
+    $fh   = fopen("testfile.txt", 'r+') or die("Failes to open file");
+    $text = fgets($fh);
+
+    fseek($fh, 0, SEEK_END);
+    fwrite($fh, "\n$text") or die("Could not write to file");
+    fclose($fh);
+
+    echo "File 'testfile.txt' successfully updated";
+?>
